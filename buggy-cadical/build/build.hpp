bc4f9c9751b5fc53341c2ccba79484914cb42f66
Marienus Heule <mheule@SCS003B8.SP.CS.CMU.EDU>
2022-09-28 18:32:09 -0400

    cadical

---
+++ buggy-cadical/build/build.hpp
@@ -0,0 +1,5 @@
+#define VERSION "1.5.2"
+#define IDENTIFIER "1250714e257cc45ba73a3cc2c34472e3210f91f6"
+#define COMPILER "Apple clang version 13.1.6 (clang-1316.0.21.2.5)"
+#define FLAGS "-Wall -Wextra -O3 -DNDEBUG -std=c++0x"
+#define DATE "Sun Sep 11 11:41:26 EDT 2022 Darwin SCS003B8.SP.CS.CMU.EDU 21.6.0 x86_64"
